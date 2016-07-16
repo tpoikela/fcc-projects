3dd0c12365033dd69b099698f8775bc6a29e9d87
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2016-07-16 07:42:43 +0200

    Added utility test functions into their own file.

---
+++ RogueLike/roguetest.js
@@ -0,0 +1,24 @@
+
+
+var chai = require("chai");
+var expect = chai.expect;
+
+var RGTest = {
+
+};
+
+RGTest.checkActorXY = function(actor, x, y) {
+    expect(actor.getX()).to.equal(x);
+    expect(actor.getY()).to.equal(y);
+};
+
+
+if ( typeof exports !== 'undefined' ) {
+    if( typeof RGTest !== 'undefined' && module.exports ) {
+        exports = module.exports = RGTest;
+    }
+    exports.RGTest = RGTest;
+}
+else {
+    window.RGTest = RGTest;
+}
