08f31eea0408ff8ab20b8a314e72c2e0bd4013e1
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2016-07-28 23:18:52 +0200

    Added tests for the event pool.

--- RogueLike/tests/events.js
+++ RogueLike/tests/events.js
@@ -1,15 +1,31 @@
 
 
 
+var chai = require("chai");
+var expect = chai.expect;
+var RG = require("../roguelike.js");
 
+EventPool = RG.EventPool;
 
-var listener = {
-    notify: function(name, args) {
-        console.log("Event " + name + " notified. Args:");
-        for (var p in args) {
-            console.log(p + " -> " + args[p]);
+var Listener = function(eventName) {
+
+    this.notified =  false;
+
+    this.eventName = eventName;
+
+    this.notify = function(name, args) {
+        if (name === this.eventName) {
+            console.log("Event " + name + " notified. Args:");
+            for (var p in args) {
+                console.log(p + " -> " + args[p]);
+            }
+            this.notified = true;
         }
-    },
+    };
+
+    this.clearNotify = function() {
+        this.notified = false;
+    };
 };
 
 var emitter = {
@@ -18,18 +34,29 @@ var emitter = {
     emit: function(name, args) {
         pool.emitEvent(name, args);
     },
+
 };
 
 var empty = {
 
 };
 
+var listener = new Listener("ActualEvent");
 var pool = new EventPool();
-pool.listenEvent("ActualEvent", listener);
+pool.listenEvent(listener.eventName, listener);
 pool.listenEvent("TestEvent", empty);
 emitter.pool = pool;
 emitter.emit("TestEvent", {data: "abcd"});
-emitter.emit("ActualEvent", {data: "abcd"});
 
+describe('How events bubble in the system', function() {
+    it('Sends notification to listener', function() {
+        expect(listener.notified).to.equal(false);
+        emitter.emit("ActualEvent", {data: "abcd"});
+        expect(listener.notified).to.equal(true);
+        listener.clearNotify();
+        expect(listener.notified).to.equal(false);
+        emitter.emit("RandomEvent", {data: "abcd"});
 
+    });
+});
 
