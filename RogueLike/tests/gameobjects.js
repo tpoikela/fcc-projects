646cfd4d6d6f3b6f81c06239f660160d0e1876ee
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2016-08-01 00:02:17 +0200

    Added unit tests for random retrieval methods from db.

--- RogueLike/tests/gameobjects.js
+++ RogueLike/tests/gameobjects.js
@@ -52,7 +52,7 @@ describe('How actors are created from file', function() {
         expect(createdWolf.getHP()).to.equal(wolfObj.getHP());
 
         var player = RG.FACT.createPlayer("player", {});
-        player.setType("actors");
+        player.setType("player");
         player.setIsPlayer(true);
         var cell = new RG.FACT.createFloorCell();
         cell.setProp("actors", player);
@@ -61,8 +61,18 @@ describe('How actors are created from file', function() {
         var actorChar = RG.charStyles.actors.player;
         expect(RG.getCellChar(cell)).to.equal(actorChar);
 
-        var randWolf = parser.createRandomActor(3);
+        var randWolf = parser.createRandomActor({danger: 3});
         expect(randWolf.getAttack()).to.equal(superWolf.attack);
+
+        var punyWolf = parser.parseObj("actors", {name: "Puny wolf",
+            base: "wolf", attack: 1, defense: 50}
+        );
+
+        var punyWolfCreated = parser.createRandomActor({
+            func: function(actor) {return actor.attack < 2;}
+        });
+        expect(punyWolfCreated.getDefense()).to.equal(50);
+        expect(punyWolfCreated.getAttack()).to.equal(1);
 
     });
 });
@@ -71,7 +81,7 @@ describe('How items are created from objects', function() {
    var parser = new Parser();
     it('description', function() {
         var foodBase = parser.parseObj("items", {type: "food", name: "foodBase",
-        weight: 0.1});
+        weight: 0.1, misc: "XXX", dontCreate: true, "char": "%"});
 
         var food = parser.parseObj("items", {base: "foodBase", name: "Dried meat",
             energy: 100, value: 5
@@ -82,9 +92,53 @@ describe('How items are created from objects', function() {
         expect(food.type).to.equal("food");
         expect(food.weight).to.equal(0.1);
 
+        var expFood = parser.parseObj("items", {name: "Gelee", energy: 500, 
+            weight: 0.2, value: 100, base: "foodBase"});
+
+        var geleeObj = parser.dbGet({name: "Gelee"})[0];
+        expect(geleeObj.char).to.equal("%");
+
+        var items = parser.dbGet({categ: "items"});
+        expect(Object.keys(items).length).to.equal(2);
+
+        var randFood = parser.createRandomItem({
+            func: function(item) {return item.value <= 5;}
+        });
+
+        expect(randFood.getEnergy()).to.equal(100);
+        expect(randFood.getValue()).to.equal(5);
+
+        var geleeFood = parser.createRandomItem({
+            func: function(item) {return item.value >= 99;}
+        });
+        expect(geleeFood.getEnergy()).to.equal(500);
+        expect(geleeFood.getType()).to.equal("food");
+
+        var testCell = RG.FACT.createFloorCell();
+        testCell.setExplored(true);
+        testCell.setProp("items", geleeFood);
+        expect(RG.getCellChar(testCell)).to.equal("%");
     });
 });
 
 //---------------------------------------------------------------------------
-// DATABASE TESTS
+// PARSING THE FULL OBJECTS FILE
 //---------------------------------------------------------------------------
+
+describe('It contains all game content info', function() {
+    var parser = new Parser();
+    parser.parseData(Obs);
+
+    it('Should parse all actors properly', function() {
+        var rsnake = parser.get("actors", "rattlesnake");
+        expect(rsnake.poison).to.equal(true);
+        var coyote = parser.get("actors", "coyote");
+        expect(coyote.attack).to.equal(3);
+        expect(coyote.danger).to.equal(2);
+    });
+
+
+    it('Should parse all items properly', function() {
+    });
+});
+
