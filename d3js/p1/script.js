a4aaaeaf06537c0912577811d64998f82850e180
Tuomas Sakari Poikela <tuomas.sakari.poikela@cern.ch>
2016-10-03 13:11:19 +0200

    Shows now the difference and percentual increase when two bars are clicked.

--- d3js/p1/script.js
+++ d3js/p1/script.js
@@ -19,6 +19,8 @@ var margin = {top: 20, left: 50, right: 40, bottom: 50};
 
 var yearToDate = {};
 
+var dataPair = [];
+
 var dateToYM = function(date) {
     var ymd = date.split("-");
     var year = ymd[0];
@@ -33,6 +35,13 @@ var dateToYM = function(date) {
 var tooltip = d3.select("body")
     .append("div")
     .classed("my-tooltip", true)
+    .style("position", "absolute")
+    .style("z-index", "10")
+    .style("visibility", "hidden");
+
+var comparisonBox = d3.select("body")
+    .append("div")
+    .classed("comparison-box", true)
     .style("position", "absolute")
     .style("z-index", "10")
     .style("visibility", "hidden");
@@ -108,17 +117,85 @@ var processGDPData = function(data) {
             .attr("width", scaleX.bandwidth())
             .attr("x", function(d, i) {return scaleX(d.year)})
             .attr("y", function(d, i) {return scaleY(d.val);})
+
+            .on("mousedown", function(d, i) {
+                var x = d3.event.pageX;
+                var y = d3.event.pageY;
+
+                d.x = x;
+                d.y = y;
+
+                if (dataPair.length === 2) {
+                    dataPair = [];
+                }
+                dataPair.unshift(d);
+
+                if (dataPair.length === 2) {
+                    var d1 = dataPair[0];
+                    var d2 = dataPair[1];
+
+                    var x1 = d1.x;
+                    var y1 = d1.y;
+                    var x2 = d2.x;
+                    var y2 = d2.y;
+                    var ym1 = dateToYM(d1.date);
+                    var ym2 = dateToYM(d2.date);
+
+                    var timeSpan = "";
+                    var percent = 0;
+
+                    if (d1.year < d2.year) {
+                        var years = d2.year - d1.year;
+                        var diff = d2.val - d1.val;
+                        var perYear = diff / years;
+                        timeSpan = "<p>From: " + ym1 + "</p><p>To: " + ym2 + "</p>";
+                        percent = Math.log2(d2.val/d1.val) / years;
+                    }
+                    else {
+                        var years = d1.year - d2.year;
+                        var diff = d1.val - d2.val;
+                        var perYear = diff / years;
+                        timeSpan = "<p>From: " + ym2 + "<br/>To: " + ym1 + "</p>";
+                        percent = Math.log2(d1.val/d2.val) / years;
+                    }
+
+                    percent *= 100;
+
+                    var boxHTML = timeSpan + "<p>Diff: " + diff + " billion USD<br/>" +
+                        "Yearly: " + perYear.toFixed(2) + 
+                        "(" + percent.toFixed(2) + "%) billion USD</p>";
+
+                    comparisonBox.style("top", bHeightMax/2 +"px")
+                        .style("left", bWidthMax-50 +"px");
+                    comparisonBox.html(boxHTML);
+                    return comparisonBox.style("visibility", "visible");
+                }
+                else if (dataPair.length === 1) {
+                    var ym = dateToYM(d.date);
+                    var boxHTML = "<p>" + ym + "</p>";
+                    comparisonBox.style("top", bHeightMax/2 +"px")
+                        .style("left", bWidthMax-50 +"px");
+                    comparisonBox.html(boxHTML);
+                    return comparisonBox.style("visibility", "visible");
+                }
+                else {
+                    return comparisonBox.style("visibility", "hidden");
+                }
+            })
+
             .on("mouseover", function(d, i) {
                 var yearMonth = dateToYM(d.date);
                 tooltip.html('<p>' + yearMonth +
                     "</p><p class='dollars'>$" + d.val + " billion</p>");
                 return tooltip.style("visibility", "visible");
             })
+
             .on("mousemove", function(d, i) {
                 return tooltip
                     .style("top", (d3.event.pageY-10)+"px")
                     .style("left",(d3.event.pageX+10)+"px");
             })
+
             .on("mouseout", function(){
                 return tooltip.style("visibility", "hidden");
             });
