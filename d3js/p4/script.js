1682b29c5c4ca8d29ca4cbbbcd827f8975f47666
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2016-10-24 23:51:13 +0200

    First graph with all countries is now plotted. Flags are missing and dragging doesn't work yet.

--- d3js/p4/script.js
+++ d3js/p4/script.js
@@ -9,12 +9,47 @@ var data_url = "https://raw.githubusercontent.com/DealPete/forceDirected/master/
 
 var margin = {top: 10, left: 10, right: 80, bottom: 30};
 
+var nodeRadius = 25;
+
 var processCountryData = function(data) {
     var svg    = d3.select("svg");
     var width  = parseInt(svg.attr("width"));
     var height = parseInt(svg.attr("height"));
 
+	var nodes = data.nodes;
+	var links = data.links;
 
+	var force = d3.layout.force()
+		.size([width, height])
+		.nodes(nodes)
+		.links(links);
+
+	var link = svg.selectAll('.link')
+		.data(links)
+		.enter().append('line')
+		.attr('class', 'link');
+
+	var node = svg.selectAll('.node')
+		.data(nodes)
+		.enter().append('circle')
+		.attr('class', 'node');
+
+    force.on('end', function() {
+
+        // Add the image for each flag here
+        node.attr('r', nodeRadius)
+            .attr('cx', function(d) { return d.x; })
+            .attr('cy', function(d) { return d.y; })
+            .text(function(d) {return d.code;});
+
+        link.attr('x1', function(d) { return d.source.x; })
+            .attr('y1', function(d) { return d.source.y; })
+            .attr('x2', function(d) { return d.target.x; })
+            .attr('y2', function(d) { return d.target.y; });
+
+    });
+
+	force.start();
 
 };
 
